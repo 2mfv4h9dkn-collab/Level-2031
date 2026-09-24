d23344add94e64d4e238b64230140bbc56e61fe8
2mfv4h9dkn-collab <2mfv4h9dkn@privaterelay.appleid.com>
2026-09-23 21:21:53 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='level2031-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
